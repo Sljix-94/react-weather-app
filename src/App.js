902f76eff2efa6bf1d14112116cb79794d03e0ee
Djordje Sljivancanin <djordje.sljivancanin@hotmail.com>
2021-05-22 20:59:11 +0200

    Switch betwen units

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect,useCallback } from "react";
 import classes from "./App.module.css";
 import axios from "axios";
 import Weather from "./Components/Weather";
@@ -7,14 +7,14 @@ function App() {
   const [weatherInfo, setWeatherInfo] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  const fetchData = async () => {
+  const [unit,setUnit] = useState('metric');
+ 
+  const fetchWeatherData = useCallback(async (unit = "metric") => {
     const key = "95662109ff51268dddd80880a65ffd09";
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${key}&units=metric`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${key}&units=${unit}`;
     axios
       .get(url)
       .then((response) => {
-        console.log(response.data);
         const {
           name,
           main: { temp: temperature },
@@ -31,15 +31,25 @@ function App() {
       })
       .catch((error) => setError("Something went wrong!"));
     setLoading(false);
+  },[]);
+  const changeUnitHandler = () => {
+    setUnit((prevState)=>{
+      return prevState === 'metric' ? 'imperial' : 'metric'
+    })
+    
+    fetchWeatherData(unit === 'metric' ? 'imperial' : 'metric');
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchWeatherData();
+  }, [fetchWeatherData]);
 
   let content;
   if (loading) content = <p>Loading...</p>;
-  if (weatherInfo) content = <Weather weatherInfo={weatherInfo} />;
+  if (weatherInfo)
+    content = (
+      <Weather weatherInfo={weatherInfo} changeUnit={changeUnitHandler} unit = {unit}/>
+    );
   if (error) content = <p>{error}</p>;
 
   return <main className={classes.container}>{content}</main>;
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,17 +1,18 @@
 import classes from "./Weather.module.css";
 
 const Weather = (props) => {
+    
   return (
     <div className={classes.container}>
       <div className={classes.header}>
-        <h1>Europe/{props.weatherInfo.name}</h1>
+        <h1 onClick = {props.changeUnit}>Europe/{props.weatherInfo.name}</h1>
         <img
           src={`http://openweathermap.org/img/wn/${props.weatherInfo.icon}.png`}
           alt="Icon"
         />
       </div>
       <div className={classes.temperature}>
-        {props.weatherInfo.temperature} C
+        {props.weatherInfo.temperature} {props.unit === 'metric' ? 'C' : 'F'}
       </div>
       <div>{props.weatherInfo.description}</div>
     </div>
