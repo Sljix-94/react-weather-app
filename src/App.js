d9467cd5d26b3e4cf22480dd948d731249f2e3b9
Djordje Sljivancanin <djordje.sljivancanin@hotmail.com>
2021-05-22 17:37:15 +0200

    created fetchData function base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import classes from "./App.module.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const fetchData = async () => {
+    const key = "95662109ff51268dddd80880a65ffd09";
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${key}&units=metric`;
+    axios
+      .get(url)
+      .then((response) => {
+        return console.log(response.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <main className={classes.container}>CONTENT</main>;
 }
 
 export default App;
