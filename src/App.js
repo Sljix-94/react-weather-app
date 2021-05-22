ce3e28322dd288145860e351109cc122383d6478
Djordje Sljivancanin <djordje.sljivancanin@hotmail.com>
2021-05-22 18:12:09 +0200

    destructuring object from fetched data and set state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import classes from "./App.module.css";
 import axios from "axios";
 
 function App() {
+  const [weatherInfo, setWeatherInfo] = useState(null);
+
   const fetchData = async () => {
     const key = "95662109ff51268dddd80880a65ffd09";
     const url = `http://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${key}&units=metric`;
     axios
       .get(url)
       .then((response) => {
-        return console.log(response.data);
+        console.log(response.data);
+        const {
+          name,
+          main: { temp: temperature },
+          weather: [{ description }],
+          weather: [{ icon }],
+        } = response.data;
+
+        setWeatherInfo({
+          name,
+          temperature,
+          description,
+          icon,
+        });
       })
       .catch((error) => console.log(error));
   };
